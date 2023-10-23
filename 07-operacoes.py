ac89686167f195c62b9a5593dace1cf957040a76
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2023-10-22 23:00:58 -0300

    fist commit

---
+++ 07-operacoes.py
@@ -0,0 +1,19 @@
+gameDescription = """
+Fifa 23 é um Jogo de Futebol
+"""
+
+gameName = "Fifa"
+gameVersion = " 23"
+line = "="
+
+# 1 - Operação de concatenação de Strings
+print(line * 25)
+gameFullName = gameName + gameVersion
+print(gameFullName)
+
+# 2 - Operação de multiplicação de Stings
+print(line * 25)
+
+# 3 - Procura palavra dentro de um texto
+print("Fifa" in gameDescription)
+print("Futebol" in gameDescription)
